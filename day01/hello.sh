02d76f216d8ad866b7b54e7355c62f353971cd5a
Ubuntu <ubuntu@ip-172-31-2-62.ap-south-1.compute.internal>
2026-09-19 22:08:45 +0000

    added day 01

---
+++ day01/hello.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# THIS IS A FIRST SHELL SCRIPT (COMBINATIONS OF SHELL COMMANDS)
+
+echo "I am very happy and excited to learn"
+
+echo "Ok now going to explore and learn the 
+      shell scripting and goint to make this
+      content in the file called first-shell" | tee first-shell
+
+   # Now checking the memory
+
+   free -m
+
+   # Now checking the disk usage
+   
+   du -h
+
+   # Now checking the free disk
+    
+    df -h
+
+   # CPU usage
+    top
